17ca82cbbdc9241deaa8fb489153e13268daa604
Omarkojak <omarkojaks@gmail.com>
2017-03-01 00:41:08 +0200

    chore: first setup trial of app.js file

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const DB_Url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/miniproject';
+const port = process.env.port || 5000;
+
+const app = express();
+mongoose.connect(DB_Url);
+
+app.listen(port, function () {
+    console.log(`Server Listening On Port ` + port);
+});
